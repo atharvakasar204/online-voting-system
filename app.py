7f7e12fb259dc6de7f16602c44890fa36db83b5b
Atharva Kasar <160533488+atharvakasar204@users.noreply.github.com>
2026-05-14 18:58:11 +0000

    Fix Render Python version

--- app.py
+++ app.py
@@ -683,15 +683,11 @@ def logout():
 # ---------------- RUN ----------------
 if __name__ == '__main__':
 
-if __name__ == '__main__':
-
     port = int(
         os.environ.get("PORT", 5000)
     )
 
     app.run(
-
         host='0.0.0.0',
         port=port
-
     )
